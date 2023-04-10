a35d6dc27325c56a3307117402e3633ac5242752
Franco de Guzman <tangkekofranco06@gmail.com>
2023-04-10 17:53:34 +0800

    button designs done

--- components/MainGame.vue
+++ components/MainGame.vue
@@ -1,51 +1,44 @@
 <template>
-    <div class="main-container-game">
-      <div class="header">
-
-        <a v-b-modal.modal-multi-game-settings><i class="fa-solid fa-gears"></i></a>
-        
-      </div>
-      <div class="main-content">
-        <div class="grid">
-        <div class="left"> 
-           <div class="board">
-              <b-row v-for="row in numRows" :key="row">
-                  <b-col v-for="cols in numCols" 
-                    :key="cols"
-                    :data-row="row"
-                    :data-column="cols"
-                    class="col-design cell-data">
-                   
-                  </b-col>
-              </b-row>
-           </div>
+  <div class="main-container-game">
+    <div class="header">
+      <a v-b-modal.modal-multi-game-settings><i class="fa-solid fa-gears"></i></a>
+    </div>
+    <div class="main-content">
+      <div class="grid">
+        <div class="left">
+          <div class="board">
+            <b-row v-for="row in numRows" :key="row">
+              <b-col v-for="cols in numCols" :key="cols" :data-row="row" :data-column="cols" class="col-design cell-data">
+              </b-col>
+            </b-row>
+          </div>
         </div>
         <div class="right">
           <h1>{{minutes}}:{{ seconds }}</h1>
           <b-container>
             <b-row class="text-center">
-              <b-col v-for="word in words" :key="word" cols="4" >
+              <b-col v-for="word in words" :key="word" cols="4">
                 {{word}}
               </b-col>
             </b-row>
           </b-container>
         </div>
       </div>
-      <b-modal id="modal-multi-game-settings"  no-close-on-backdrop centered hide-footer hide-header>
-          <template #default="{ close }">
-            <b-button @click="close()" block pill variant="success">
-              <h3>CONTINUE</h3>
+      <b-modal id="modal-multi-game-settings" no-close-on-backdrop centered hide-footer hide-header>
+        <template #default="{ close }">
+              <b-button @click="close()" block class="button-play">
+                <h3>CONTINUE</h3>
+              </b-button>
+            
+            <b-button v-b-modal.modal-multi-2 block class="button-quit-game">
+              <h3>QUIT GAME</h3>
             </b-button>
-          
-          <b-button v-b-modal.modal-multi-2 block pill variant="danger">
-            <h3>QUIT GAME</h3>
-          </b-button>
-        </template>
+</template>
         </b-modal>
 
         <b-modal id="modal-multi-2" no-close-on-backdrop centered hide-footer>
           <h2>Are you sure?</h2>
-          <b-button v-b-modal.modal-multi-1 size="sm" block pill variant="danger">
+          <b-button v-b-modal.modal-multi-1 size="sm" block class="button-quit-game">
             <h3>QUIT GAME</h3>
           </b-button>
         </b-modal>
@@ -54,15 +47,15 @@
 </template>
 
 <script>
-export default {
-  props:['numCols','numRows','words'],
-  data(){
-    return{
-      timerCount: 120, //In Seconds
-      minutes: 2,
-      seconds: "00",
-      remainingWords : 10,
-      tempWords: []
+  export default {
+    props: ['numCols', 'numRows', 'words'],
+    data() {
+      return {
+        timerCount: 120, //In Seconds
+        minutes: 2,
+        seconds: "00",
+        remainingWords: 10,
+        tempWords: []
       }
     },
     watch: {
@@ -77,100 +70,88 @@ export default {
             }, 1000);
           }
         },
-        immediate: true 
-    }
-  },
-  methods:{
-    checkCellOccupied(word, startPosition, orientation){
+        immediate: true
+      }
+    },
+    methods: {
+      checkCellOccupied(word, startPosition, orientation) {
         var status = ''
-        var incrementBy = 0 
+        var incrementBy = 0
         const individuals = document.querySelectorAll('.cell-data');
-        if(orientation === 'row'){
+        if (orientation === 'row') {
           incrementBy = 1
-        }
-        else if(orientation === 'column'){
+        } else if (orientation === 'column') {
           incrementBy = this.numCols
         }
-        for(let p = startPosition,q=0; q<word.length; q++){
-          if(individuals[p].hasAttribute('data-word')){
+        for (let p = startPosition, q = 0; q < word.length; q++) {
+          if (individuals[p].hasAttribute('data-word')) {
             status = 'occupied'
             break
-          }
-          else{
+          } else {
             status = 'empty'
           }
-          p+= incrementBy
+          p += incrementBy
         }
         return status
-    },
-    
-    placeCorrectWords(wordsArray){
-      var positions = ['row', 'column']
-      var nextLetter = 0
-      var newStartPoint = 0
-      for(let i = 0 ; i<wordsArray.length; i++){
-        var getOrientation = positions[Math.floor(Math.random()*positions.length)] 
-        const individuals = document.querySelectorAll('.cell-data');
-        const start = Math.floor(Math.random()*individuals.length)
-        
-        const startRow = individuals[start].getAttribute('data-row');
-        
-        const startColumn = individuals[start].getAttribute('data-column');
-    
-        if(getOrientation === 'row'){
-          nextLetter = 1 
-          if((startColumn*1)+ wordsArray[i].length <= this.numCols){
-            newStartPoint = start
+      },
+      placeCorrectWords(wordsArray) {
+        var positions = ['row', 'column']
+        var nextLetter = 0
+        var newStartPoint = 0
+        for (let i = 0; i < wordsArray.length; i++) {
+          var getOrientation = positions[Math.floor(Math.random() * positions.length)]
+          const individuals = document.querySelectorAll('.cell-data');
+          const start = Math.floor(Math.random() * individuals.length)
+          const startRow = individuals[start].getAttribute('data-row');
+          const startColumn = individuals[start].getAttribute('data-column');
+          if (getOrientation === 'row') {
+            nextLetter = 1
+            if ((startColumn * 1) + wordsArray[i].length <= this.numCols) {
+              newStartPoint = start
+            } else {
+              var newColumnPoint = this.numCols - wordsArray[i].length - 1
+              newStartPoint = ((startRow - 1) * this.numCols) + (newColumnPoint - 1)
+            }
+          } else if (getOrientation === 'column') {
+            nextLetter = this.numCols
+            if ((startRow * 1) + wordsArray[i].length <= this.numRows) {
+              newStartPoint = start
+            } else {
+              var newRowPoint = this.numRows - wordsArray[i].length - 1
+              newStartPoint = newRowPoint * startColumn
+            }
           }
-          else{
-            var newColumnPoint = this.numCols - wordsArray[i].length - 1
-            newStartPoint = ((startRow-1)*this.numCols)+(newColumnPoint-1)
-          }
-        }
-        else if(getOrientation === 'column'){
-          nextLetter = this.numCols
-          if((startRow*1)+ wordsArray[i].length <= this.numRows){
-            newStartPoint = start
-          }
-          else{
-            var newRowPoint = this.numRows - wordsArray[i].length - 1
-            newStartPoint = newRowPoint * startColumn
-          }
-        }
-        var characters = wordsArray[i].split("");
-        var nextPosition = 0
-        var isCellOccupied = this.checkCellOccupied(wordsArray[i], newStartPoint, getOrientation)
-        if(isCellOccupied === 'empty'){
+          var characters = wordsArray[i].split("");
+          var nextPosition = 0
+          var isCellOccupied = this.checkCellOccupied(wordsArray[i], newStartPoint, getOrientation)
+          if (isCellOccupied === 'empty') {
             characters.forEach(item => {
-            individuals[newStartPoint + nextPosition].innerText = item
-            individuals[newStartPoint + nextPosition].setAttribute('data-word', wordsArray[i])
-            individuals[newStartPoint + nextPosition].style.color = "red";
-            nextPosition += nextLetter;
-          });
+              individuals[newStartPoint + nextPosition].innerText = item
+              individuals[newStartPoint + nextPosition].setAttribute('data-word', wordsArray[i])
+              individuals[newStartPoint + nextPosition].style.color = "red";
+              nextPosition += nextLetter;
+            });
+          } else {
+            this.tempWords.push(wordsArray[i])
+          }
         }
-        else{
-          this.tempWords.push(wordsArray[i])
+      },
+      generateRandomLetter() {
+        const individuals = document.querySelectorAll('.cell-data');
+        var alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        for (let i = 0; i < individuals.length; i++) {
+          if (!individuals[i].hasAttribute('data-word')) {
+            individuals[i].innerText = alphabets.charAt(Math.floor(Math.random() * 26))
+          }
         }
-      }
+      },
     },
-
-    generateRandomLetter(){
-      const individuals = document.querySelectorAll('.cell-data');
-      var alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-      for(let i=0; i<individuals.length;i++){
-        if(!individuals[i].hasAttribute('data-word')){
-            individuals[i].innerText = alphabets.charAt(Math.floor(Math.random()*26))
-        }
-      }
+    mounted() {
+      this.placeCorrectWords(this.words)
+      this.placeCorrectWords(this.tempWords)
+      this.generateRandomLetter()
     },
-    
-  },
-  mounted(){
-    this.placeCorrectWords(this.words)
-    this.placeCorrectWords(this.tempWords)
-    this.generateRandomLetter()
-  },
-}
+  }
 </script>
 
 
@@ -219,7 +200,6 @@ export default {
     /* border: 1px solid red; */
     width: 100%;
   }
- 
   .col-design {
     border: 1px solid black;
     width: 40px;
@@ -238,7 +218,74 @@ export default {
   a i {
     font-size: 2em;
   }
-  #modal-multi-2 h2{
+  #modal-multi-2 h2 {
     text-align: center;
+  }
+  /* BUTTONS CSS */
+  button {
+    height: 60px;
+    border-radius: 15px;
+    border: none;
+    color: #fff;
+    transition: 0.3s ease;
+    font-weight: 800;
+    font-family: 'Coiny', cursive;
+    font-size: 2em;
+    -webkit-text-stroke: .5px #000000;
+  }
+  .button-play {
+    background: linear-gradient(#3ffa6e 25%, #0ae641 50%);
+    border-radius: 10px;
+    margin-bottom: 25px;
+    box-shadow: 0 10px #3ffa6e;
+  }
+  .button-play:hover {
+    box-shadow: 0 8px #3ffa6e;
+    transform: translateY(1px);
+  }
+  .button-play:active {
+    box-shadow: 0 5px #3ffa6e;
+    transform: translateY(4px);
+  }
+  .button-settings {
+    background: linear-gradient(#6d9fb6 25%, #397691 50%);
+    border-radius: 10px;
+    margin-bottom: 25px;
+    box-shadow: 0 10px #6d9fb6;
+  }
+  .button-settings:hover {
+    box-shadow: 0 8px #6d9fb6;
+    transform: translateY(1px);
+  }
+  .button-settings:active {
+    box-shadow: 0 5px #6d9fb6;
+    transform: translateY(4px);
+  }
+  .button-show-credits {
+    background: linear-gradient(#6d9fb6 25%, #397691 50%);
+    border-radius: 10px;
+    margin-bottom: 25px;
+    box-shadow: 0 10px #6d9fb6;
+  }
+  .button-show-credits:hover {
+    box-shadow: 0 8px #6d9fb6;
+    transform: translateY(1px);
+  }
+  .button-show-credits:active {
+    box-shadow: 0 5px #6d9fb6;
+    transform: translateY(4px);
+  }
+  .button-quit-game {
+    background: linear-gradient(#f75362 25%, #dc3545 50%);
+    border-radius: 10px;
+    box-shadow: 0 10px #f6727e;
+  }
+  .button-quit-game:hover {
+    box-shadow: 0 8px #f6727e;
+    transform: translateY(1px);
+  }
+  .button-quit-game:active {
+    box-shadow: 0 5px #f6727e;
+    transform: translateY(4px);
   }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,27 @@
 <template>
-  <div class="main-container">
-      <transition name="home">
-          <MainGame v-if="playInit" :numCols="numCols" :numRows="numRows" :words="words"/>
-      </transition>
-      <div v-if="!playInit" class="menu">
-          <h1>WORD FIND</h1>
-          <div v-if="!playInit" class="button-class">
-              <b-button @click="playInit= true" pill class="button-design-play">Play</b-button>
-              <b-button v-b-modal.modal-multi-settings pill class="button-design-settings">Settings</b-button>
-          </div>
-      
-          <b-modal id="modal-multi-settings" size="lg" hide-footer no-stacking centered>
-              <template #modal-title><h1>SETTINGS</h1></template>
+    <div class="main-container">
+        <transition name="home">
+            <MainGame v-if="playInit" :numCols="numCols" :numRows="numRows" :words="words" />
+        </transition>
+        <div v-if="!playInit" class="menu">
+            <h1>WORD FIND</h1>
+            <div v-if="!playInit" class="button-class">
+                <b-button @click="playInit= true" class="button-play">Play</b-button>
+                <b-button v-b-modal.modal-multi-settings class="button-settings">Settings</b-button>
+            </div>
+            <b-modal id="modal-multi-settings" size="lg" hide-footer no-stacking centered>
+                <template #modal-title><h1>SETTINGS</h1>
+</template>
                   <p class="my-2">CHOOSE THEME: 
                       <b-button v-b-modal.modal-multi-theme></b-button>
                   </p>
-              <b-button class="mt-3" block pill v-b-modal.modal-multi-credits><h3>SHOW CREDITS</h3></b-button>
+              <b-button class="mt-3 button-show-credits" block v-b-modal.modal-multi-credits><h3>SHOW CREDITS</h3></b-button>
           </b-modal>
 
           <b-modal id="modal-multi-theme" hide-footer centered>
-              <template #modal-title><h1>CHOOSE THEME</h1></template>
+<template #modal-title>
+    <h1>CHOOSE THEME</h1>
+</template>
               <b-container fluid>
                   <b-row>
                   <b-col v-for="(item, index) in themes" :key="index" cols="12" sm="6" md="4" class="mb-3">
@@ -31,7 +33,9 @@
           </b-modal>
 
           <b-modal id="modal-multi-credits" hide-footer centered>
-              <template #modal-title><h1>CREDITS</h1></template>
+<template #modal-title>
+    <h1>CREDITS</h1>
+</template>
               <b-container fluid>
                   <b-row>
                   <b-col v-for="(item, index) in credits" :key="index" cols="12" sm="6" md="4" class="mb-3">
@@ -47,119 +51,174 @@
 </template>
 
 <script>
-  export default {
-      data() {
-          return {
-              playInit: false,
-              numCols: 14,
-              numRows: 12,
-              words: ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi', 'lemon'],
-              credits: [
-                  { name: 'William', image: 'https://via.placeholder.com/100' },
-                  { name: 'Franco', image: 'https://via.placeholder.com/100' },
-                  { name: 'Austin', image: 'https://via.placeholder.com/100' },
-              ],
-              themes: [
-                  { name: 'Theme 1', image: 'https://via.placeholder.com/100' },
-                  { name: 'Theme 2', image: 'https://via.placeholder.com/100' },
-                  { name: 'Theme 3', image: 'https://via.placeholder.com/100' },
-                  { name: 'Theme 4', image: 'https://via.placeholder.com/100' },
-                  { name: 'Theme 5', image: 'https://via.placeholder.com/100' },
-                  { name: 'Theme 6', image: 'https://via.placeholder.com/100' },
-              ],
-          }
-      },
-      methods: {}
-  }
+    export default {
+        data() {
+            return {
+                playInit: false,
+                numCols: 14,
+                numRows: 12,
+                words: ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grape', 'honeydew', 'kiwi', 'lemon'],
+                credits: [{
+                        name: 'William',
+                        image: 'https://via.placeholder.com/100'
+                    },
+                    {
+                        name: 'Franco',
+                        image: 'https://via.placeholder.com/100'
+                    },
+                    {
+                        name: 'Austin',
+                        image: 'https://via.placeholder.com/100'
+                    },
+                ],
+                themes: [{
+                        name: 'Theme 1',
+                        image: 'https://via.placeholder.com/100'
+                    },
+                    {
+                        name: 'Theme 2',
+                        image: 'https://via.placeholder.com/100'
+                    },
+                    {
+                        name: 'Theme 3',
+                        image: 'https://via.placeholder.com/100'
+                    },
+                    {
+                        name: 'Theme 4',
+                        image: 'https://via.placeholder.com/100'
+                    },
+                    {
+                        name: 'Theme 5',
+                        image: 'https://via.placeholder.com/100'
+                    },
+                    {
+                        name: 'Theme 6',
+                        image: 'https://via.placeholder.com/100'
+                    },
+                ],
+            }
+        },
+        methods: {}
+    }
 </script>
  
 
 <style scoped>
-  .home-enter-active,
-  .home-leave-active {
-      transition: opacity .5s;
-  }
-  .home-enter,
-  .home-leave-active {
-      opacity: 0;
-  }
-  .main-container {
-      display: flex;
-      flex-direction: column;
-      min-height: 100vh;
-      width: 100%;
-      justify-content: center;
-      align-items: center;
-      padding: 0;
-      margin: 0;
-      background-image: url("~/static/background-1.png");
-      background-position: center;
-      background-repeat: no-repeat;
-      background-size: cover;
-  }
-  .menu h1 {
-      font-family: 'Bangers', cursive;
-      font-size: 190px;
-      color: white;
-      text-shadow: 2px 2px 4px #000000;
-  }
-  .menu .button-class {
-      display: flex;
-      justify-content: center;
-      gap: 1em;
-      width: 100%;
-  }
-  /* pakiedit ng design */
-  .menu .button-design-play {
-      background-color: rgb(73, 248, 73);
-      font-family: 'Coiny', cursive;
-      font-weight: 800;
-      font-size: 2em;
-      width: 20%;
-      padding: 8px 2px;
-      text-shadow: 2px 2px 4px #000000;
-      box-shadow: 2px 2px 4px #000000;
-  }
-  /* pakiedit ng design */
-  .menu .button-design-play:hover {}
-  /* pakiedit ng design */
-  .menu .button-design-settings {
-      background-color: #397691;
-      font-family: 'Coiny', cursive;
-      font-weight: 800;
-      border: 2px solid #397691;
-      font-size: 2em;
-      padding: 8px 2px;
-      width: 30%;
-      text-shadow: 2px 2px 4px #000000;
-      box-shadow: 2px 2px 4px #000000;
-  }
-  /* pakiedit ng design */
-  .menu .button-design-settings:hover {}
-  .menu .button-design-diff {
-      background-color: rgb(73, 248, 73);
-      font-family: 'Coiny', cursive;
-      font-weight: 800;
-      font-size: 2em;
-      width: 20%;
-      padding: 8px 2px;
-      text-shadow: 2px 2px 4px #000000;
-      box-shadow: 2px 2px 4px #000000;
-  }
-  /* pakiedit ng design */
-  .menu .button-design-play:hover {}
-  .menu .difficulty {
-      display: flex;
-      gap: 1em;
-      justify-content: center;
-  }
-  .modal-header {
-  display: flex;
-  align-items: flex-start;
-  justify-content: space-between;
-  padding: 1rem 1rem;
-  border-bottom: none !important;
-  border-top-left-radius: calc(0.3rem - 1px);
-  border-top-right-radius: calc(0.3rem - 1px);
-  }
+    .home-enter-active,
+    .home-leave-active {
+        transition: opacity .5s;
+    }
+    .home-enter,
+    .home-leave-active {
+        opacity: 0;
+    }
+    .main-container {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+        width: 100%;
+        justify-content: center;
+        align-items: center;
+        padding: 0;
+        margin: 0;
+        background-image: radial-gradient(#7DF9FF, #1F30DD);
+        box-shadow: inset 0 0 100px hsla(0, 0%, 0%, .3);
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+    .menu h1 {
+        font-family: 'Bangers', cursive;
+        font-size: 190px;
+        color: white;
+        text-shadow: 2px 2px 4px #000000;
+    }
+    .menu .button-class {
+        display: flex;
+        justify-content: center;
+        gap: 1em;
+        width: 100%;
+    }
+    /* pakiedit ng design */
+    .menu .button-design-play {
+        background-color: rgb(73, 248, 73);
+        font-family: 'Coiny', cursive;
+        font-weight: 800;
+        font-size: 2em;
+        width: 20%;
+        padding: 8px 2px;
+        text-shadow: 2px 2px 4px #000000;
+        box-shadow: 2px 2px 4px #000000;
+    }
+    .menu .difficulty {
+        display: flex;
+        gap: 1em;
+        justify-content: center;
+    }
+    .modal-header {
+        display: flex;
+        align-items: flex-start;
+        justify-content: space-between;
+        padding: 1rem 1rem;
+        border-bottom: none !important;
+        border-top-left-radius: calc(0.3rem - 1px);
+        border-top-right-radius: calc(0.3rem - 1px);
+    }
+    button {
+        height: 60px;
+        border-radius: 15px;
+        border: none;
+        color: #fff;
+        -webkit-text-stroke: .1px #000000;
+        transition: 0.3s ease;
+        font-weight: 800;
+        font-family: 'Coiny', cursive;
+        font-size: 2em;
+    }
+    .button-play {
+        padding: 2px 30px 2px 30px;
+        background: linear-gradient(#3ffa6e 25%, #0ae641 50%);
+        border-radius: 10px;
+        margin-bottom: 25px;
+        box-shadow: 0 10px #3ffa6e;
+    }
+    .button-play:hover {
+        box-shadow: 0 8px #3ffa6e;
+        transform: translateY(1px);
+    }
+    .button-play:active {
+        box-shadow: 0 5px #3ffa6e;
+        transform: translateY(4px);
+    }
+    .button-settings {
+        padding: 2px 30px 2px 30px;
+        background: linear-gradient(#6d9fb6 25%, #397691 50%);
+        border-radius: 10px;
+        margin-bottom: 25px;
+        box-shadow: 0 10px #6d9fb6;
+    }
+    .button-settings:hover {
+        box-shadow: 0 8px #6d9fb6;
+        transform: translateY(1px);
+    }
+    .button-settings:active {
+        box-shadow: 0 5px #6d9fb6;
+        transform: translateY(4px);
+    }
+    .button-show-credits {
+        padding: 2px 30px 2px 30px;
+        background: linear-gradient(#6d9fb6 25%, #397691 50%);
+        border-radius: 10px;
+        margin-bottom: 25px;
+        box-shadow: 0 10px #6d9fb6;
+        -webkit-text-stroke: .8px #000000;
+    }
+    .button-show-credits:hover {
+        box-shadow: 0 8px #6d9fb6;
+        transform: translateY(1px);
+    }
+    .button-show-credits:active {
+        box-shadow: 0 5px #6d9fb6;
+        transform: translateY(4px);
+    }
 </style>
